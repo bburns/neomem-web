e08135a5b820648038325c61754afc8fd86b39d3
Brian Burns <bburns.km@gmail.com>
2020-08-17 03:46:38 -0500

    wip orgfiles; start docview

---
+++ src/app/index.jsx
@@ -0,0 +1,235 @@
+import React from 'react'
+import neo4jDatasource from '../datasources/neo4jDatasource'
+// import TableView from '../views/TableView'
+import DocumentView from '../views/DocumentView'
+import logo from '../assets/logo256.png'
+import './styles.css'
+
+
+//. these will need to get translated from generic datastructs into
+// those specific to each datasource
+
+const projectsQuery = `
+MATCH (n:Project) 
+OPTIONAL MATCH (n)-[:CLIENT]->(c)
+WITH n, labels(n) as type, id(n) as id,
+collect(c.name) as client
+RETURN n { .*, type, id, client }
+`
+const projectsCols = "id,type,name,description,client"
+const projectsAddQuery = `
+CREATE (n:Project)
+WITH n, labels(n) as type, id(n) as id
+RETURN n { .*, type, id }
+`
+
+const projectQuery = `
+MATCH (n)-[r:PROJECT]->(m:Project {name:$projectName}) 
+OPTIONAL MATCH (n)-[:TIMEFRAME]->(t:Timeframe)
+WITH n, labels(n) as type, collect(t.name) as timeframe, 
+collect(m.name) as project, 
+id(n) as id
+RETURN n { .*, type, timeframe, project, id }
+`
+const projectCols = "id,project,type,name,timeframe,description"
+const projectAddQuery = `
+MATCH (m:Project {name:$projectName})
+CREATE (n:Task)-[:PROJECT]->(m)
+WITH n, labels(n) as type, id(n) as id
+RETURN n { .*, type, id }
+`
+
+const genericQuery = `
+MATCH (n:#label#) 
+WITH n, labels(n) as type, id(n) as id
+RETURN n { .*, type, id }
+`
+const genericCols = "id,type,name,description"
+const genericAddQuery = `
+CREATE (n:#label#)
+WITH n, labels(n) as type, id(n) as id
+RETURN n { .*, type, id }
+`
+
+const facetObjs = {
+  facets: {
+    params: { label: 'Facet' },
+    query: genericQuery,
+    addQuery: genericAddQuery,
+    cols: genericCols,
+  },
+  all: {
+    params: {},
+    query: `
+    MATCH (n) 
+    WITH n, labels(n) as type, id(n) as id
+    RETURN n { .*, type, id }
+    `,
+    cols: genericCols,
+    addQuery: genericAddQuery,
+  },
+  projects: {
+    params: {},
+    query: projectsQuery,
+    cols: projectsCols,
+    addQuery: projectsAddQuery,
+  },
+  personal: { params: { projectName: 'personal' }, query: projectQuery, cols: projectCols, addQuery: projectAddQuery },
+  neomem: { params: { projectName: 'neomem' }, query: projectQuery, cols: projectCols, addQuery: projectAddQuery },
+  tallieo: { params: { projectName: 'tallieo' }, query: projectQuery, cols: projectCols, addQuery: projectAddQuery },
+  facemate: { params: { projectName: 'facemate' }, query: projectQuery, cols: projectCols, addQuery: projectAddQuery },
+  ccs: { params: { projectName: 'ccs' }, query: projectQuery, cols: projectCols, addQuery: projectAddQuery },
+  people: {
+    params: { label: 'Person' },
+    query: genericQuery,
+    cols: genericCols,
+    addQuery: genericAddQuery,
+  },
+  books: {
+    query: `
+    MATCH (n) 
+    WHERE (n:Book) or (n:Author) 
+    OPTIONAL MATCH (n)-[r:AUTHOR]->(m) 
+    WITH n, collect(m.name) as author, labels(n) as type, id(n) as id
+    RETURN n { .*, type, author, id }`,
+    cols: "id,type,author,name,description",
+  },
+  timeframe: {
+    //. include items without a project also
+    query: `
+    MATCH (n)-[r:PROJECT]->(m), (n)-[:TIMEFRAME]->(t) 
+    WITH n, labels(n) AS type, collect(m.name) AS project , collect(t.name) AS timeframe, id(n) as id
+    RETURN n {.*, type, project, timeframe, id }`,
+    cols: "id,type,project,name,timeframe,description",
+  },
+}
+
+function substituteQueryParams(query, params) {
+  for (const key of Object.keys(params)) {
+    const value = params[key]
+    query = query.replace('#' + key + '#', value)
+  }  
+  return query
+}
+
+
+
+const emptyRow = { id:0 }
+
+
+export default function App() {
+
+  const [facet, setFacet] = React.useState("projects")
+  const [groupBy, setGroupBy] = React.useState("")
+  const [view, setView] = React.useState("table")
+  // const [query, setQuery] = React.useState("") // used to display query to user
+  const [rows, setRows] = React.useState([])
+  const [columns, setColumns] = React.useState([])
+  const tableRef = React.useRef(null)
+  const facetRef = React.useRef(facet) //. better way?
+
+  React.useEffect(() => {
+    facetRef.current = facet
+    ;(async () => {
+      const facetObj = facetObjs[facet]
+      let { query, params={}, cols } = facetObj
+      query = substituteQueryParams(query, params)
+      // setQuery(query)
+
+      // const colNames = cols.split(',')
+      // const columns = colNames.map(colName => colDefs[colName])
+      // setColumns(columns)
+
+      if (!query) return
+      console.log(query, params)
+      const rows = []
+      // const session = driver.session({ defaultAccessMode: neo4j.session.READ })
+      const session = neo4jDatasource.getSession()
+      const result = await session.run(query, params)
+      result.records.forEach(record => {
+        const row = record.get('n')
+        Object.keys(row).forEach(key => {
+          if (Array.isArray(row[key])) {
+            row[key] = row[key].join(', ')
+          }
+        })
+        rows.push(row)
+      })
+      rows.push(emptyRow)
+      session.close()
+      setRows(rows)
+    })()
+  }, [facet])
+
+  function changeFacet(e) {
+    const facet = e.currentTarget.value
+    setFacet(facet)
+  }
+
+  function changeGroupBy(e) {
+    const groupBy = e.currentTarget.value
+    setGroupBy(groupBy)
+    const table = tableRef.current.table
+    table.setGroupBy(groupBy)
+  }
+
+  function changeView(e) {
+    const view = e.currentTarget.value
+    setView(view)
+  }
+
+  return (
+    <div className="app">
+      
+      <div className="app-header">
+        
+        <div className="app-header-logo">
+          <img src={logo} alt="logo" /> 
+          <span>Neomem</span>
+        </div>
+
+        <div className="app-controls">
+          <span className="app-controls-facet">
+            <span>Facet:&nbsp;</span>
+            <select name="facet" id="facet" value={facet} onChange={changeFacet}>
+              {Object.keys(facetObjs).map(facet => <option key={facet} value={facet}>{facet}</option>)}
+            </select>
+          </span>
+          <span className="app-controls-groupby">
+            <span>Group:&nbsp;</span>
+            <select name="groupby" id="groupby" value={groupBy} onChange={changeGroupBy}>
+              <option value="">(none)</option>
+              <option value="type">type</option>
+              <option value="timeframe">timeframe</option>
+              <option value="project">project</option>
+              <option value="client">client</option>
+            </select>
+          </span>
+          <span className="app-controls-view">
+            <span>View:&nbsp;</span>
+            <select name="view" id="view" value={view} onChange={changeView}>
+              <option value="table">table</option>
+              <option value="document">document</option>
+              <option value="outline">outline</option>
+            </select>
+          </span>
+        </div>
+        {/* <div className="app-header-query">Query: {query}</div> */}
+      </div>
+      
+      <div className="app-contents">
+        {/* <TableView 
+          data={rows}
+          options={{groupBy}}
+          columns={columns}
+          // tooltips={false}
+          // layout={"fitData"}
+          // cellEdited={cellEdited}
+          // // dataEdited={newData => console.log('dataEdited', newData)}
+          // // footerElement={<span>Footer</span>}
+        /> */}
+        <DocumentView />
+      </div>
+    </div>
+  )
+}
---
+++ src/datasources/neo4jDatasource/index.js
@@ -0,0 +1,48 @@
+import neo4j from 'neo4j-driver'
+
+
+// get neo4j driver
+const uri = process.env.REACT_APP_NEO4J_URI
+const user = process.env.REACT_APP_NEO4J_USER
+const password = process.env.REACT_APP_NEO4J_PASSWORD
+// note: neo4j stores 64-bit ints, js only goes up to 53-bits (9e16)
+// see https://github.com/neo4j/neo4j-javascript-driver#enabling-native-numbers
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password), { disableLosslessIntegers: true })
+
+// export default driver
+
+function getSession(readOnly=false) {
+  if (readOnly) {
+    const session = driver.session({ defaultAccessMode: neo4j.session.READ })
+    return session
+  }
+  const session = driver.session()
+  return session
+}
+
+
+export default {
+  getSession,
+}
+
+// ;(async function() {
+//   const session = driver.session()
+//   const query = "call db.labels()"
+//   const results = await session.run(query)
+//   console.log(results)
+//   const foo = results.records.map(record => record.get('label')).sort()
+//   console.log(foo)
+//   session.close()
+// })()
+
+async function getTypes() {
+  const session = driver.session()
+  const query = "call db.labels()"
+  const results = await session.run(query)
+  session.close()
+  const types = results.records.map(record => record.get('label')).sort()
+  return types
+}
+
+
+
