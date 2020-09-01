e0c937c9d931116d51b902e0b1804a7742f3f943
Brian Burns <bburns.km@gmail.com>
2020-09-01 10:31:03 -0500

    mv files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import * as serviceWorker from './lib/serviceWorker'
+import * as serviceWorker from './services/serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
