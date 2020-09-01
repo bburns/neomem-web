97234d63db3cf05a06750d0d82d4cba5cb9d58c0
Brian Burns <bburns.km@gmail.com>
2020-09-01 16:51:14 -0500

    wip

--- src/plugins/views/navigator/index.jsx
+++ src/plugins/views/navigator/index.jsx
@@ -4,10 +4,14 @@ import './styles.css'
 export default function NavigatorView({ items }) {
   return (
     <div className='navigator-view'>
-      <div>Navigator</div>
+      <div className='navigator-view-header'>Navigator</div>
+      <div className="navigator-view-items">
         {items && items.map((item, i) => (
-          <div className='navigator-view-item' key={item.name + i}>{item.name}</div>
+          <div className='navigator-view-item' key={item.name + i}>
+            {item.name}
+          </div>
         ))}
+      </div>
     </div>
   )
 }
